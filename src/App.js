2ecf710e69957ed08d8926e83b4848689dcdd4f1
yijungai <ethan.yijun@gmail.com>
2020-11-26 21:55:19 +1100

    delete not used code

--- src/App.js
+++ src/App.js
@@ -86,13 +86,6 @@ const onDragEnd = (result, columns, setColumns) => {
 
 
 function App() {
-  // const itemsFromBackend = [
-  //   { id: uuid(), content: "First task" },
-  //   { id: uuid(), content: "Second task" },
-  //   { id: uuid(), content: "Third task" },
-  //   { id: uuid(), content: "Fourth task" },
-  //   { id: uuid(), content: "Fifth task" }
-  // ];
 
 
   const [itemsFromBackend, setItemsFromBackend] = useState([
